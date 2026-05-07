44cbf18ca31bb7a1f9e011be855b26457466c2e3
Sarahmm21 <445052163@std.psau.edu.sa>
2026-05-07 17:58:20 +0300

    protect shered executionLog using lock and unlock

--- SchedulerSimulationSync.java
+++ SchedulerSimulationSync.java
@@ -90,7 +90,13 @@ class SharedResources {
     public static void logExecution(String message) {
         // TODO: Protect this critical section with a lock
         // RACE CONDITION: ArrayList is not thread-safe!
-        executionLog.add(message);
+        lock.lock();
+        try {
+            executionLog.add(message);
+        } finally {
+            lock.tryLock();
+        }
+
     }
 }
 
